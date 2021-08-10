b67387912635558e75617e17378b67973e18ee56
Pranjali53 <80103087+Pranjali53@users.noreply.github.com>
2021-08-10 13:02:52 +0530

    Add files via upload

---
+++ w3Resource.py
@@ -0,0 +1,11 @@
+nums = [23, 22, 44, 17, 77, 55, 1, 65, 82, 2]
+# new=max(nums)
+# print(new)
+Max=[]
+for i in nums:
+    for n in nums:
+        if nums[i]>nums[n]:
+            Max=nums[i]
+    print(MAx)
+
+
